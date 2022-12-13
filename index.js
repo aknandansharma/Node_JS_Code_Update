8f153d7faaf918583076f215c519c8661d697aa9
Aknandan Sharma <90132050+aknandansharma@users.noreply.github.com>
2022-12-14 02:13:14 +0530

    OS Module To Get Operating System.

--- index.js
+++ index.js
@@ -1,74 +1,29 @@
-const fs = require("fs");
+// OS Module To Get Operating System.
 
-                                                        /*   All Operation Is Synchronous */
+const os = require("node:os");
 
-// Create a file named "hello.txt":
-// fs.writeFileSync("hello.txt", "Hello from Node.js!");
+// this is console your computer Articture  .
+console.log(os.arch()) //--> output x64AM
 
-// fs.appendFileSync("hello.txt", "Hello from Node.js! by Aknandan Sharma. Auranganbad Bihar");
+// Show your free RAM
+const freeMemory = os.freemem();
+// console.log(freeMemory)
+console.log(`${freeMemory/1024/1024/1024}`)
 
-// const buf_data = fs.readFileSync("hello.txt");
-// console.log(buf_data);
-
-// Output: <Buffer 48 65 6c 6c 6f 20 66 72 6f 6d 20 4e 6f 64 65 2e 6a 73 21 0a 48 65 6c 6c 6f 20 66 72 6f 6d 20 4e 6f 64 65 2e 6a 73 21 20 62 79 20 41 6b 6e 61 6e 64 61 6e 20 53 68 61 72 6d 61 2e 20 41 75 72 61 6e 67 61 6e 62 61 64 20 42 69 68 61 72 0a>
-
-// const org_data = buf_data.toString();
-// console.log(org_data);
-
-// fs.renameSync("hello.txt", "readwrite.txt");
-
-// fs.writeFileSync("hello.txt", "Hello from Node.js! This is first operation.");
-
-// fs.appendFileSync("nandan/hello.txt", "Hello from Node.js! by Aknandan Sharma. Auranganbad Bihar This is Bihari pokharhan Aurangabad :-)");
+// Show your Total Memoey
+const totelMemory = os.totalmem();
+// console.log(totelMemory)
+console.log(`${totelMemory / 1024 / 1024 / 1024}`);
 
 
-// const buf_data = fs.readFileSync("nandan/hello.txt", "utf-8");
-// console.log(buf_data);
+// PC User Name
+console.log(os.hostname());
 
-// const org_data = buf_data.toString();
-// console.log(org_data);
+// PC Type Like -> Windows, Linux...etc
+console.log(os.platform());
 
-// fs.renameSync("nandan/hello.txt", "nandan/readwrite.txt");
-
-// fs.unlinkSync("nandan/readwrite.txt");
-
-// fs.rmdirSync("nandan");
-
-// fs.mkdir("AsyncFile")
+// Somthing
+console.log(os.tmpdir());
+console.log(os.type());
 
 
-
-                                                        /*   All Operation Is Asynchronous */    
-
-
-// File Creating
-// fs.mkdir("asyncFile" , (err)=>{
-//     console.log("File is created")
-// })      
-
-// Inside asyncFile read.text created
-// fs.writeFile("asyncFile/read.txt", "This file is created!", (err) => {
-//   console.log("text file is created");
-// });
-
-
-// read.text data updating 
-// fs.appendFile("asyncFile/read.txt", "This file is created!\n data updating", (err) => {
-//   console.log("text file is data is complited.");
-// });
-
-
-// Reading the data
-// fs.readFile("asyncFile/read.txt", 'utf-8', (err, data) => {
-//   console.log(data);
-// });
-
-// file name change
-// fs.rename("asyncFile/read.txt", "asyncFile/readFile.txt", (err) => {
-//   console.log("File Name Changed");
-// });
-
-// file name change
-// fs.rmdir("asyncFile", (err) => {
-//   console.log("File Deleted");
-// });
